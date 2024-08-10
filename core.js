a7df2e9f8c9ee08f325e501544cbf5e6c0c18d71
softsingh <softsingh@jagandeep.com>
2024-08-10 16:51:05 +0530

    wip

--- core.js
+++ core.js
@@ -29,13 +29,40 @@ sidebarToggle.addEventListener('click', ()=>{
    body.classList.toggle('sidebar-hidden')
 })
 
-navbarToggle.addEventListener('click', ()=>{
-   body.classList.toggle('sidebar-hidden')
-})
-
 // Toggle Sidebar visibility when Screen Overlay is clicked
 const screenOverlay = document.querySelector('.screen-overlay');
 
 screenOverlay.addEventListener('click', ()=>{
    document.body.classList.toggle('sidebar-hidden');
+});
+
+navbarToggle.addEventListener('click', ()=>{
+   body.classList.toggle('sidebar-hidden')
+})
+
+searchInputWrapper = document.querySelector('.navbar .search-input-wrapper');
+searchInput = document.querySelector('.navbar .search-input-wrapper .search-input');
+
+searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', ()=> {
+   searchInputWrapper.classList.remove('d-none');
+   searchInput.focus();
+   searchInput.value='';
+})
+
+searchCloser = document.querySelector('.search-closer');
+searchCloser.addEventListener('click', ()=> {
+   searchInputWrapper.classList.add('d-none');
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' || event.ctrlKey && event.key === '/') {
+    searchInputWrapper.classList.add('d-none');
+  }
+
+  if (event.ctrlKey && event.key === '/') {
+    searchInputWrapper.classList.remove('d-none');
+    searchInput.focus();
+    searchInput.value='';
+  }
 });

26f2fdb0e2815fbfbe505164f2216ba8d0665b62
Softsingh <softsingh@github.com>
2024-08-15 21:22:18 +0530

    wip

--- core.js
+++ core.js
@@ -6,7 +6,7 @@ if (window.innerWidth <= 768) {
 
 function clearMenuItemOpenActive(currentMenuItem) {
    menuItems.forEach(menuItem => {
-      if(menuItem !== currentMenuItem) {
+      if (menuItem !== currentMenuItem) {
          menuItem.classList.remove('active');
          menuItem.classList.remove('open')
       }
@@ -15,7 +15,7 @@ function clearMenuItemOpenActive(currentMenuItem) {
 
 const menuItems = document.querySelectorAll('.menu-item');
 
-menuItems.forEach(menuItem => menuItem.addEventListener('click', ()=>{
+menuItems.forEach(menuItem => menuItem.addEventListener('click', () => {
    clearMenuItemOpenActive(menuItem);
    menuItem.classList.add('active');
    menuItem.classList.toggle('open')
@@ -25,18 +25,18 @@ const body = document.querySelector('body');
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 const navbarToggle = document.querySelector('.navbar-toggle');
 
-sidebarToggle.addEventListener('click', ()=>{
+sidebarToggle.addEventListener('click', () => {
    body.classList.toggle('sidebar-hidden')
 })
 
 // Toggle Sidebar visibility when Screen Overlay is clicked
 const screenOverlay = document.querySelector('.screen-overlay');
 
-screenOverlay.addEventListener('click', ()=>{
+screenOverlay.addEventListener('click', () => {
    document.body.classList.toggle('sidebar-hidden');
 });
 
-navbarToggle.addEventListener('click', ()=>{
+navbarToggle.addEventListener('click', () => {
    body.classList.toggle('sidebar-hidden')
 })
 
@@ -44,62 +44,72 @@ searchInputWrapper = document.querySelector('.navbar .search-input-wrapper');
 searchInput = document.querySelector('.navbar .search-input-wrapper .search-input');
 
 searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', (event)=> {
-   searchInputWrapper.classList.add('show');
-   searchInput.focus();
-   searchInput.value='';
+searchButton.addEventListener('click', (event) => {
+   showNavbarSearchInput();
    event.stopPropagation();
 })
 
 searchCloser = document.querySelector('.search-closer');
-searchCloser.addEventListener('click', ()=> {
+searchCloser.addEventListener('click', () => {
    searchInputWrapper.classList.remove('show');
 })
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape' || event.ctrlKey && event.key === '/') {
-    searchInputWrapper.classList.remove('show');
-  }
+   if (event.key === 'Escape' || event.ctrlKey && event.key === '/') {
+      searchInputWrapper.classList.remove('show');
+   }
 
-  if (event.ctrlKey && event.key === '/') {
-    searchInputWrapper.classList.add('show');
-    searchInput.focus();
-    searchInput.value='';
-  }
+   if (event.ctrlKey && event.key === '/') {
+      showNavbarSearchInput()
+   }
 });
 
 // Dropdown Menu
 const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
 dropdownToggles.forEach((dropdownToggle) => {
-   dropdownToggle.addEventListener('click',(event) => {
+   dropdownToggle.addEventListener('click', (event) => {
       const parent = event.currentTarget.parentElement;
       const dropdownMenu = parent.querySelector('.dropdown-menu');
+      hideAllDropdowns();
       dropdownMenu.classList.toggle('show');
       event.stopPropagation();
    })
 })
 
 //Click on empty area
-document.addEventListener('click', (event) => {
+document.documentElement.addEventListener('click', (event) => {
 
-   // Close any of the open dropdown-menu
-   const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-
-   dropdownMenus.forEach((dropdownMenu) => {
-      if(dropdownMenu.classList.contains('show')) {
-         if(event.currentTarget !== dropdownMenu) {
-            //alert(event.currentTarget.classList)
-            dropdownMenu.classList.remove('show');
-         }
-      }
-   })
+   // Hide all shown Dropdown Menus
+   hideAllDropdowns();
 
    // Close Navbar search-input
-   if(searchInputWrapper.classList.contains('show')) {
-      //alert(event.target.ClassList)
-      if(event.target !== searchInput) {
+   if (searchInputWrapper.classList.contains('show')) {
+      if (event.target !== searchInput) {
          searchInputWrapper.classList.remove('show');
       }
    }
 })
+
+function showNavbarSearchInput() {
+   // Hide all shown Dropdown Menus
+   hideAllDropdowns();
+
+   searchInputWrapper.classList.add('show');
+   searchInput.focus();
+   searchInput.value = '';
+}
+
+// Hide all shown Dropdown Menus
+function hideAllDropdowns() {
+
+   const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+
+   dropdownMenus.forEach((dropdownMenu) => {
+      if (dropdownMenu.classList.contains('show')) {
+         if (event.currentTarget !== dropdownMenu) {
+            dropdownMenu.classList.remove('show');
+         }
+      }
+   })
+}

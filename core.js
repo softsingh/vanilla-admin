a808ad4808e0a31a8b30af0381740572d9539a82
softsingh <softsingh@jagandeep.com>
2024-09-17 17:27:59 +0530

    Modal WIP

--- core.js
+++ core.js
@@ -1,282 +1,310 @@
-
 // Hide Sidebar by default on small screens
 if (window.innerWidth <= 768) {
-   document.body.classList.add('sidebar-hidden');
+  document.body.classList.add('sidebar-hidden');
 }
 
 // Accordions
-const accordionHeaders = document.querySelectorAll('.accordion .accordion-header');
+const accordionHeaders = document.querySelectorAll(
+  '.accordion .accordion-header'
+);
 
-accordionHeaders.forEach(accordionHeader => accordionHeader.addEventListener('click', (event) => {
-   const curentAccordionItem = accordionHeader.closest('.accordion-item');
-   const parentName = curentAccordionItem.getAttribute('parent');
+accordionHeaders.forEach((accordionHeader) =>
+  accordionHeader.addEventListener('click', (event) => {
+    const curentAccordionItem = accordionHeader.closest('.accordion-item');
+    const parentName = curentAccordionItem.getAttribute('parent');
 
-   if (parentName !== '') {
+    if (parentName !== '') {
       const parent = document.getElementById(parentName);
       if (parent) {
-         const accordionItems = parent.querySelectorAll('.accordion-item');
-         accordionItems.forEach((accordionItem) => {
-            if (accordionItem !== curentAccordionItem) {
-               // Only close accordion-items having parent value equal to accordion id
-               if (accordionItem.getAttribute('parent') === parentName) {
-                  accordionItem.classList.remove('open')
-               }
+        const accordionItems = parent.querySelectorAll('.accordion-item');
+        accordionItems.forEach((accordionItem) => {
+          if (accordionItem !== curentAccordionItem) {
+            // Only close accordion-items having parent value equal to accordion id
+            if (accordionItem.getAttribute('parent') === parentName) {
+              accordionItem.classList.remove('open');
             }
-         });
+          }
+        });
       }
-   }
+    }
 
-   curentAccordionItem.classList.toggle('open')
-   event.stopPropagation();
-}));
+    curentAccordionItem.classList.toggle('open');
+    event.stopPropagation();
+  })
+);
 
 // Alert Dismissible btn-close
-const alertDismissButtons = document.querySelectorAll('.alert-dismissible .btn-close');
+const alertDismissButtons = document.querySelectorAll(
+  '.alert-dismissible .btn-close'
+);
 
-alertDismissButtons.forEach(alertDismissButton => alertDismissButton.addEventListener('click', (event) => {
-   const alertDismiss = alertDismissButton.closest('.alert-dismissible');
-   alertDismiss.classList.add('d-none');
-   event.stopPropagation();
-}))
-
+alertDismissButtons.forEach((alertDismissButton) =>
+  alertDismissButton.addEventListener('click', (event) => {
+    const alertDismiss = alertDismissButton.closest('.alert-dismissible');
+    alertDismiss.classList.add('d-none');
+    event.stopPropagation();
+  })
+);
 
 // Sidebar Menu
 const menuItems = document.querySelectorAll('.menu>.menu-item');
 
-menuItems.forEach(menuItem => menuItem.addEventListener('click', (event) => {
-   clearMenuItemOpenActive(menuItem);
-   menuItem.classList.add('active');
-   menuItem.classList.toggle('open');
-   event.stopPropagation();
-}))
+menuItems.forEach((menuItem) =>
+  menuItem.addEventListener('click', (event) => {
+    clearMenuItemOpenActive(menuItem);
+    menuItem.classList.add('active');
+    menuItem.classList.toggle('open');
+    event.stopPropagation();
+  })
+);
 
 // Sidebar Sub-Menu
 const menuSubItems = document.querySelectorAll('.menu-sub>.menu-item');
 
-menuSubItems.forEach(menuSubItem => menuSubItem.addEventListener('click', (event) => {
-   clearMenuSubItemActive(menuSubItem);
+menuSubItems.forEach((menuSubItem) =>
+  menuSubItem.addEventListener('click', (event) => {
+    clearMenuSubItemActive(menuSubItem);
 
-   const menuSubSub = menuSubItem.querySelectorAll('.menu-sub');
+    const menuSubSub = menuSubItem.querySelectorAll('.menu-sub');
 
-   if (menuSubSub.length > 0) {
+    if (menuSubSub.length > 0) {
       menuSubItem.classList.toggle('open');
-   }
-   else {
+    } else {
       menuSubItem.classList.add('active');
-   }
+    }
 
-   event.stopPropagation();
-}))
+    event.stopPropagation();
+  })
+);
 
 const body = document.querySelector('body');
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 const navbarToggle = document.querySelector('.navbar-toggle');
 
 sidebarToggle.addEventListener('click', () => {
-   body.classList.toggle('sidebar-hidden')
-})
+  body.classList.toggle('sidebar-hidden');
+});
 
 // Toggle Sidebar visibility when Screen Overlay is clicked
 const screenOverlay = document.querySelector('.screen-overlay');
 
 screenOverlay.addEventListener('click', () => {
-   document.body.classList.toggle('sidebar-hidden');
+  document.body.classList.toggle('sidebar-hidden');
 });
 
 navbarToggle.addEventListener('click', () => {
-   body.classList.toggle('sidebar-hidden')
-})
+  body.classList.toggle('sidebar-hidden');
+});
 
 searchInputWrapper = document.querySelector('.navbar .search-input-wrapper');
-searchInput = document.querySelector('.navbar .search-input-wrapper .search-input');
+searchInput = document.querySelector(
+  '.navbar .search-input-wrapper .search-input'
+);
 
 searchButton = document.querySelector('.search-button');
 searchButton.addEventListener('click', (event) => {
-   showNavbarSearchInput();
-   event.stopPropagation();
-})
+  showNavbarSearchInput();
+  event.stopPropagation();
+});
 
 searchCloser = document.querySelector('.search-closer');
 searchCloser.addEventListener('click', () => {
-   searchInputWrapper.classList.remove('show');
-})
+  searchInputWrapper.classList.remove('show');
+});
 
 document.addEventListener('keydown', (event) => {
-   if (event.key === 'Escape' || event.ctrlKey && event.key === '/') {
-      searchInputWrapper.classList.remove('show');
-   }
+  if (event.key === 'Escape' || (event.ctrlKey && event.key === '/')) {
+    searchInputWrapper.classList.remove('show');
+  }
 
-   if (event.ctrlKey && event.key === '/') {
-      showNavbarSearchInput()
-   }
+  if (event.ctrlKey && event.key === '/') {
+    showNavbarSearchInput();
+  }
 });
 
 // Dropdown Menu
 const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
 dropdownToggles.forEach((dropdownToggle) => {
-   dropdownToggle.addEventListener('click', (event) => {
-      const parent = event.currentTarget.parentElement;
-      const dropdownMenu = parent.querySelector('.dropdown-menu');
-      hideAllDropdowns();
-      dropdownMenu.classList.toggle('show');
-      event.stopPropagation();
-   })
-})
-
-// Dropdown Notifications Menu
-const notificationReads = document.querySelectorAll('.dropdown-notifications-read');
-
-notificationReads.forEach((notificationRead) => {
-   notificationRead.addEventListener('click', (event) => {
-      const notificationsItem = event.currentTarget.closest(".dropdown-notifications-item");
-      notificationsItem.classList.toggle('mark-as-read');
-      event.stopPropagation();
-   })
-})
-
-const notificationArchives = document.querySelectorAll('.dropdown-notifications-archive');
-
-notificationArchives.forEach((notificationArchive) => {
-   notificationArchive.addEventListener('click', (event) => {
-      const notificationsItem = event.currentTarget.closest(".dropdown-notifications-item");
-      notificationsItem.style.display = 'none';
-      event.stopPropagation();
-   })
-})
-
-// Modal
-const modalToggles = document.querySelectorAll('[data-toggle="modal"]');
-modalToggles.forEach(modalToggle => modalToggle.addEventListener('click', (event) => {
-   const modalName = modalToggle.getAttribute('data-target');
-   if(modalName) { 
-      const modal = document.getElementById(modalName);
-      if (modal) {
-         showModal(modal)
-      }
-   }
-   event.stopPropagation();
-}));
-
-// Hide modal when Modal Backdrop is clicked
-const modalBackdrop = document.getElementById('modalBackdrop');
-
-modalBackdrop.addEventListener('click', (event) => {
-   const modalName = modalBackdrop.getAttribute('data-target');
-   alert(modalName)
-   if(modalName) { 
-      const modal = document.getElementById(modalName);
-      if (modal) {
-         
-         hideModal(modal)
-      }
-   }
+  dropdownToggle.addEventListener('click', (event) => {
+    const parent = event.currentTarget.parentElement;
+    const dropdownMenu = parent.querySelector('.dropdown-menu');
+    hideAllDropdowns();
+    dropdownMenu.classList.toggle('show');
+    event.stopPropagation();
+  });
 });
 
-function showModal(modal) {
-   // Create the Backdrop div
-   const backdrop = document.createElement('div');
-   backdrop.id = 'modalBackdrop';
-   backdrop.className = 'modal-backdrop';
-   backdrop.setAttribute('data-target', modal.id);
-   document.body.appendChild(backdrop);
+// Dropdown Notifications Menu
+const notificationReads = document.querySelectorAll(
+  '.dropdown-notifications-read'
+);
 
-   // Show the modal
-   modal.classList.add('show');
+notificationReads.forEach((notificationRead) => {
+  notificationRead.addEventListener('click', (event) => {
+    const notificationsItem = event.currentTarget.closest(
+      '.dropdown-notifications-item'
+    );
+    notificationsItem.classList.toggle('mark-as-read');
+    event.stopPropagation();
+  });
+});
+
+const notificationArchives = document.querySelectorAll(
+  '.dropdown-notifications-archive'
+);
+
+notificationArchives.forEach((notificationArchive) => {
+  notificationArchive.addEventListener('click', (event) => {
+    const notificationsItem = event.currentTarget.closest(
+      '.dropdown-notifications-item'
+    );
+    notificationsItem.style.display = 'none';
+    event.stopPropagation();
+  });
+});
+
+//////////////// Modal ////////////////
+
+const modals = document.querySelectorAll('.modal');
+const modalToggles = document.querySelectorAll('[data-toggle="modal"]');
+const modalDismissToggles = document.querySelectorAll('[data-dismiss="modal"]');
+
+// Show modal if modal toggle clicked
+modalToggles.forEach((modalToggle) =>
+  modalToggle.addEventListener('click', (event) => {
+    const modalName = modalToggle.getAttribute('data-target');
+    if (modalName) {
+      const modal = document.getElementById(modalName);
+      if (modal) {
+        showModal(modal);
+      }
+    }
+    event.stopPropagation();
+  })
+);
+
+// Hide modal if modal dismiss toggle clicked
+modalDismissToggles.forEach((modalDismissToggle) =>
+  modalDismissToggle.addEventListener('click', (event) => {
+    const modal = modalDismissToggle.closest('.modal');
+    if (modal) {
+      hideModal(modal);
+    }
+    event.stopPropagation();
+  })
+);
+
+// Hide model when clicked outside model-content
+modals.forEach((modal) =>
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      hideModal(modal);
+      event.stopPropagation();
+    }
+  })
+);
+
+function showModal(modal) {
+  const backdrop = document.createElement('div');
+  backdrop.id = 'modalBackdrop';
+  backdrop.className = 'modal-backdrop';
+  backdrop.setAttribute('data-target', modal.id);
+  document.body.appendChild(backdrop);
+
+  modal.classList.add('show');
 }
 
 function hideModal(modal) {
-   // Remove the Backdrop div
-   const backdrop = document.getElementById('modalBackdrop');
-   if (backdrop) {
-      backdrop.remove();
-   }
-   // Hide the modal
-   modal.classList.remove('show');
+  const backdrop = document.getElementById('modalBackdrop');
+  if (backdrop) {
+    backdrop.remove();
+  }
+  modal.classList.remove('show');
 }
 
 // Nav
 const navItems = document.querySelectorAll('.nav>.nav-item');
 
-navItems.forEach(navItem => navItem.addEventListener('click', (event) => {
-   clearNavItemActive(navItem);
-   navItem.classList.add('active');
-   event.stopPropagation();
-}))
+navItems.forEach((navItem) =>
+  navItem.addEventListener('click', (event) => {
+    clearNavItemActive(navItem);
+    navItem.classList.add('active');
+    event.stopPropagation();
+  })
+);
 
 // Theme Toggle
 const themeToggle = document.querySelector('.theme-toggle .nav-link i');
 
 themeToggle.addEventListener('click', () => {
-   body.classList.toggle('dark-mode')
+  body.classList.toggle('dark-mode');
 
-   if (body.classList.contains('dark-mode')) {
-      themeToggle.classList.remove('bx-moon');
-      themeToggle.classList.add('bx-sun');
-   }
-   else {
-      themeToggle.classList.remove('bx-sun');
-      themeToggle.classList.add('bx-moon');
-   }
-})
+  if (body.classList.contains('dark-mode')) {
+    themeToggle.classList.remove('bx-moon');
+    themeToggle.classList.add('bx-sun');
+  } else {
+    themeToggle.classList.remove('bx-sun');
+    themeToggle.classList.add('bx-moon');
+  }
+});
 
 //Click on empty area
 document.documentElement.addEventListener('click', (event) => {
+  // Hide all shown Dropdown Menus
+  hideAllDropdowns();
 
-   // Hide all shown Dropdown Menus
-   hideAllDropdowns();
-
-   // Close Navbar search-input
-   if (searchInputWrapper.classList.contains('show')) {
-      if (event.target !== searchInput) {
-         searchInputWrapper.classList.remove('show');
-      }
-   }
-})
+  // Close Navbar search-input
+  if (searchInputWrapper.classList.contains('show')) {
+    if (event.target !== searchInput) {
+      searchInputWrapper.classList.remove('show');
+    }
+  }
+});
 
 // Show Navbar Search Box
 function showNavbarSearchInput() {
-   hideAllDropdowns();
+  hideAllDropdowns();
 
-   searchInputWrapper.classList.add('show');
-   searchInput.focus();
-   searchInput.value = '';
+  searchInputWrapper.classList.add('show');
+  searchInput.focus();
+  searchInput.value = '';
 }
 
 // Hide all shown Dropdown Menus
 function hideAllDropdowns() {
-   const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+  const dropdownMenus = document.querySelectorAll('.dropdown-menu');
 
-   dropdownMenus.forEach(dropdownMenu => {
-      if (dropdownMenu.classList.contains('show')) {
-         if (event.currentTarget !== dropdownMenu) {
-            dropdownMenu.classList.remove('show');
-         }
+  dropdownMenus.forEach((dropdownMenu) => {
+    if (dropdownMenu.classList.contains('show')) {
+      if (event.currentTarget !== dropdownMenu) {
+        dropdownMenu.classList.remove('show');
       }
-   })
+    }
+  });
 }
 
 function clearMenuItemOpenActive(currentMenuItem) {
-   menuItems.forEach(menuItem => {
-      if (menuItem !== currentMenuItem) {
-         menuItem.classList.remove('active');
-         menuItem.classList.remove('open')
-      }
-   })
+  menuItems.forEach((menuItem) => {
+    if (menuItem !== currentMenuItem) {
+      menuItem.classList.remove('active');
+      menuItem.classList.remove('open');
+    }
+  });
 }
 
 function clearMenuSubItemActive(currentMenuSubItem) {
-   menuSubItems.forEach(menuSubItem => {
-      if (menuSubItem !== currentMenuSubItem) {
-         menuSubItem.classList.remove('active');
-      }
-   })
+  menuSubItems.forEach((menuSubItem) => {
+    if (menuSubItem !== currentMenuSubItem) {
+      menuSubItem.classList.remove('active');
+    }
+  });
 }
 
 function clearNavItemActive(currentNavItem) {
-   navItems.forEach(navItem => {
-      if (navItem !== currentNavItem) {
-         navItem.classList.remove('active');
-      }
-   })
+  navItems.forEach((navItem) => {
+    if (navItem !== currentNavItem) {
+      navItem.classList.remove('active');
+    }
+  });
 }
